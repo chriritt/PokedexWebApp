47700f121d64b2b9fce20db1bc8311a3385be67a
Chris Ritter <chriswritter@gmail.com>
2023-03-14 11:45:40 -0400

    Updating Db Models : Inheritances

--- Pokedex.Db/DbModels/PokemonModel.cs
+++ Pokedex.Db/DbModels/PokemonModel.cs
@@ -4,13 +4,8 @@
 		{
 		public int IdNum { get; set; }
 		public string? PokemonName { get; set; }
-		public string? Ability { get; set; }
-		public string? move1 { get; set; }
-		public string? move2 { get; set; }
-		public string? move3 { get; set; }
-		public string? move4 { get; set; }
-		public string? HeldItem { get; set; }
-		public string? Nature { get; set; }
+		public string? PokemonType { get; set; }
+		public string? PokemonCategory { get; set; }
 		public string? Sprite { get; set; }
 		}
 	}
