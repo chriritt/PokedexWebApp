c9a2d83160ef521d06aff2a82c9e510ecb1f6cf5
Chris Ritter <chriswritter@gmail.com>
2023-03-13 22:25:33 -0400

    Working on API

---
+++ Pokedex.API/ApiModels/PokemonModel.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+
+namespace Pokedex.API.ApiModels
+    {
+    public class PokemonModel
+        {
+        public class Pokemon
+            {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+
+            [JsonProperty("url")]
+            public string Url { get; set; }
+            }
+
+
+        public class PokeApiResults
+            {
+            [JsonProperty("count")]
+            public int Count { get; set; }
+
+            [JsonProperty("next")]
+            public string Next { get; set; }
+
+            [JsonProperty("previous")]
+            public object Previous { get; set; }
+            [JsonProperty("results")]
+            public List<Pokemon> Results { get; set; }
+
+            /*public static List<Pokemon> GetPokemon(int offset)
+				{
+				var PokeCallTask = PokemonClient.PokeApiCall(offset);
+				var result = PokeCallTask.Result;
+
+				JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+				List<Pokemon> pokemonList = JsonConvert.DeserializeObject<List<Pokemon>>(jsonResponse["results"].ToString());
+				return pokemonList;*/
+            }
+        }
+    }
+
---
+++ Pokedex.API/Clients/PokemonClient.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using static Pokedex.API.ApiModels.PokemonModel;
+
+namespace Pokedex.API.Clients
+    {
+    public class PokemonClient
+        {
+        public async Task<PokeApiResults> PokemonList()
+            {
+            var pokeClient = new HttpClient();
+            var offset = 0;
+
+            var baseUrl = $"https://pokeapi.co/api/v2/pokemon?offset={offset}";
+
+            var apiResponse = await pokeClient.GetAsync(baseUrl);
+            apiResponse.EnsureSuccessStatusCode();
+            var response = await apiResponse.Content.ReadAsStringAsync();
+            var pokeApiResult = JsonConvert.DeserializeObject<PokeApiResults>(response);
+
+            return pokeApiResult;
+            }
+        }
+    }
