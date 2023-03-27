85cbf8052aaf0b39387f97ac0363d51e99f44f83
Chris Ritter <chriswritter@gmail.com>
2023-03-26 23:33:48 -0400

    Added Sprites

--- Pokedex/Data/PokemonClient.cs
+++ Pokedex/Data/PokemonClient.cs
@@ -8,7 +8,7 @@ namespace Pokedex.UI.Data
 		public static async Task<List<Pokemon>> GetPokemon()
 			{
 			HttpClient request = new HttpClient();
-			var response = await request.GetStringAsync("https://pokeapi.co/api/v2/pokemon?limit=150");
+			var response = await request.GetStringAsync("https://pokeapi.co/api/v2/pokemon?limit=1008");
 			PokemonParser poke = JsonConvert.DeserializeObject<PokemonParser>(response);
 			return poke.results;
 			}
